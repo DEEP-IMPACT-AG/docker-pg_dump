38ca3e78198ebdc205793f3748c7ccfce32a2b86
Stanislas Nanchen <stan@hood.ch>
2018-07-31 15:26:34 +0200

    encrypt dumps

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # docker-pg_dump
 
 Alpine docker image with postgres client and a dump script as entrypoint.
+
+You need to mount the public key as a file "volume". see https://www.imagescape.com/blog/2015/12/18/encrypted-postgres-backups/ for more information.
--- dump.sh
+++ dump.sh
@@ -3,4 +3,5 @@
 set -e
 
 mkdir -p /var/dump
-pg_dump -Fc -U ${PGUSER} -h ${PGHOST} ${PGDB} > /var/dump/dump-$(date +"%Y%m%d%H%M%S")
+pg_dump -Fc -U ${PGUSER} -h ${PGHOST} ${PGDB} | openssl smime -encrypt \
+	-aes256 -binary -outform DEM -out /var/dump/dump-$(date +"%Y%m%d%H%M%S").ssl /var/backup_key.pem.pub
