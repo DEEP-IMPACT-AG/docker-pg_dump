c45b6ebd7f68856b2b9804f9eb7bf6648201bbe7
Stanislas Nanchen <stan@hood.ch>
2018-08-08 16:47:20 +0200

    fix dump script

--- dump.sh
+++ dump.sh
@@ -7,4 +7,4 @@ pg_dump -Fc -U ${PGUSER} -h ${PGHOST} ${PGDB} | openssl smime -encrypt \
 	-aes256 -binary -outform DEM -out /var/dump/dump-$(date +"%Y%m%d%H%M%S").ssl /var/backup_key.pem.pub
 
 # clean up
-find /var/dump -type f -mtime +${DUMP_RETENTION} -execdir rm -- '{}' \;"
+find /var/dump -type f -mtime +${DUMP_RETENTION} -execdir rm -- '{}' \;
