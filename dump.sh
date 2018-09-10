0f30279b186821ce7f069f80b7612010badac4bb
Stanislas Nanchen <stan@hood.ch>
2018-09-10 08:34:31 +0200

    improve dump docker image to always retain at least 30 dumps

--- dump.sh
+++ dump.sh
@@ -7,4 +7,6 @@ pg_dump -Fc -U ${PGUSER} -h ${PGHOST} ${PGDB} | openssl smime -encrypt \
 	-aes256 -binary -outform DEM -out /var/dump/dump-$(date +"%Y%m%d%H%M%S").ssl /var/backup_key.pem.pub
 
 # clean up
-find /var/dump -type f -mtime +${DUMP_RETENTION} -exec rm -- '{}' \;
+cd /var/dump
+ls -t | sed -e '1,30d' | xargs -I % find . -name % -mtime +${DUMP_RETENTION} | xargs rm
+cd /
